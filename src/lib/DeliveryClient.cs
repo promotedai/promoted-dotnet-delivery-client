23b6191adb0b5568f0d1b5600efefe9efe379c3d
James Hill <james@promoted.ai>
2023-09-13 16:26:55 -0700

    base

---
+++ src/lib/DeliveryClient.cs
@@ -0,0 +1,43 @@
+﻿using Google.Protobuf;
+using System.Text;
+
+namespace Promoted.Lib
+{
+    public class DeliveryClient
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+        private static readonly JsonFormatter formatter = new JsonFormatter(JsonFormatter.Settings.Default);
+        private static readonly JsonParser parser = new JsonParser(JsonParser.Settings.Default);
+        private readonly string url;
+
+        public DeliveryClient(string url, string apiKey)
+        {
+            this.url = url;
+            httpClient.DefaultRequestHeaders.Add("x-api-key", apiKey);
+        }
+
+        public async Task<Promoted.Delivery.Response> Deliver(Promoted.Delivery.Request req)
+        {
+            try
+            {
+                // Protobuf -> JSON
+                string json = formatter.Format(req);
+
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(url, content);
+                // This throws if the request was unsuccessful.
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // JSON -> Protobuf
+                return (Promoted.Delivery.Response)parser.Parse(responseBody, Promoted.Delivery.Response.Descriptor);
+            }
+            catch (Exception ex)
+            {
+                // TODO(james): Proper fallback behavior.
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
