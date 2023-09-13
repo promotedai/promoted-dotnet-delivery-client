243c6877f92783015089c306f6b2a46f2b1291cb
James <113625718+prm-james-hill@users.noreply.github.com>
2023-09-13 16:34:12 -0700

    Checkpoint: can talk to local delivery instance (#4)

---
+++ src/exe/ManualConsumer.cs
@@ -0,0 +1,19 @@
+﻿namespace Promoted.Exe
+{
+    class ManualConsumer
+    {
+        static async Task Main(string[] args)
+        {
+            // Create the client.
+            string url = "http://localhost:9090/deliver";
+            string apiKey = "abc";
+            var client = new Promoted.Lib.DeliveryClient(url, apiKey);
+
+            // Use the client.
+            var req = new Promoted.Delivery.Request();
+            Console.WriteLine($"Request:\t{req}");
+            Promoted.Delivery.Response resp = await client.Deliver(req);
+            Console.WriteLine($"Response:\t{resp}");
+        }
+    }
+}
