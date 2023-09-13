4f13413cb0dea8fe240ce814a84c2cb296f84492
James <113625718+prm-james-hill@users.noreply.github.com>
2023-09-12 17:28:48 -0700

    Add schema submodule (#2)

--- tests/lib_tests/UnitTest1.cs
+++ tests/lib_tests/UnitTest1.cs
@@ -5,5 +5,9 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var req = new Promoted.Delivery.Request();
+        req.UserInfo = new Promoted.Common.UserInfo();
+        req.UserInfo.UserId = "test";
+        Console.WriteLine(req.UserInfo.UserId);
     }
 }
