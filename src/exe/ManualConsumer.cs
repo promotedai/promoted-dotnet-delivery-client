e9ba2ea436e9e0cda56d3abde4dbccc2a7d26d26
James Hill <james@promoted.ai>
2023-09-14 10:51:00 -0700

    base

--- src/exe/ManualConsumer.cs
+++ src/exe/ManualConsumer.cs
@@ -2,19 +2,44 @@
 {
     class ManualConsumer
     {
+        static string GetEnvironmentVariableOrThrow(string varName)
+        {
+            string? envVar = Environment.GetEnvironmentVariable(varName);
+            if (envVar == null)
+            {
+                throw new InvalidOperationException($"Environment variable {varName} is not set.");
+            }
+            return envVar;
+        }
+
         static async Task Main(string[] args)
         {
-            // Create the client.
-            string deliveryEndpoint = "http://localhost:9090/deliver";
-            string deliveryApiKey = "abc";
-            int deliveryTimeoutMillis = 100;
-            var client = new Promoted.Lib.DeliveryClient(deliveryEndpoint, deliveryApiKey, deliveryTimeoutMillis);
+            Promoted.Lib.DeliveryClient client;
+            try
+            {
+                // Create the client.
+                string deliveryEndpoint = GetEnvironmentVariableOrThrow("DELIVERY_ENDPOINT");
+                string deliveryApiKey = GetEnvironmentVariableOrThrow("DELIVERY_API_KEY");
+                int deliveryTimeoutMillis = int.Parse(GetEnvironmentVariableOrThrow("DELIVERY_TIMEOUT_MILLIS"));
+                string metricsEndpoint = GetEnvironmentVariableOrThrow("METRICS_ENDPOINT");
+                string metricsApiKey = GetEnvironmentVariableOrThrow("METRICS_API_KEY");
+                int metricsTimeoutMillis = int.Parse(GetEnvironmentVariableOrThrow("METRICS_TIMEOUT_MILLIS"));
+                client = new Promoted.Lib.DeliveryClient(deliveryEndpoint, deliveryApiKey, deliveryTimeoutMillis,
+                                                         metricsEndpoint, metricsApiKey, metricsTimeoutMillis);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to create client: {ex.Message}");
+                return;
+            }
 
             // Use the client.
             var req = new Promoted.Delivery.Request();
             Console.WriteLine($"Request:\t{req}");
             Promoted.Delivery.Response resp = await client.Deliver(req);
             Console.WriteLine($"Response:\t{resp}");
+
+            client.Dispose();
         }
     }
 }
--- src/lib/DeliveryClient.cs
+++ src/lib/DeliveryClient.cs
@@ -1,50 +1,108 @@
 ﻿using Google.Protobuf;
+using System.Collections.Concurrent;
 using System.Text;
 
 namespace Promoted.Lib
 {
-    public class DeliveryClient
+    // Disposable to try and ensure calls to metrics finish.
+    public class DeliveryClient : IDisposable
     {
-        private static readonly HttpClient httpClient = new HttpClient();
+        private static readonly HttpClient deliveryHttpClient = new HttpClient();
+        private static readonly HttpClient metricsHttpClient = new HttpClient();
+
         private static readonly JsonFormatter formatter = new JsonFormatter(JsonFormatter.Settings.Default);
         private static readonly JsonParser parser = new JsonParser(JsonParser.Settings.Default);
+        private readonly ConcurrentBag<Task> pendingTasks = new ConcurrentBag<Task>();
         private readonly string deliveryEndpoint;
+        private readonly string metricsEndpoint;
 
-        public DeliveryClient(string deliveryEndpoint, string deliveryApiKey, int deliveryTimeoutMillis)
+        public DeliveryClient(string deliveryEndpoint, string deliveryApiKey, int deliveryTimeoutMillis,
+                              string metricsEndpoint, string metricsApiKey, int metricsTimeoutMillis)
         {
             this.deliveryEndpoint = deliveryEndpoint;
-            httpClient.DefaultRequestHeaders.Add("x-api-key", deliveryApiKey);
-            httpClient.Timeout = TimeSpan.FromMilliseconds(deliveryTimeoutMillis);
+            deliveryHttpClient.DefaultRequestHeaders.Add("x-api-key", deliveryApiKey);
+            deliveryHttpClient.Timeout = TimeSpan.FromMilliseconds(deliveryTimeoutMillis);
+            this.metricsEndpoint = metricsEndpoint;
+            metricsHttpClient.DefaultRequestHeaders.Add("x-api-key", metricsApiKey);
+            metricsHttpClient.Timeout = TimeSpan.FromMilliseconds(metricsTimeoutMillis);
+
+            Console.WriteLine("Constructed delivery client:");
+            Console.WriteLine($"- Delivery endpoint = {deliveryEndpoint}");
+            Console.WriteLine($"- Metrics endpoint = {metricsEndpoint}");
         }
 
-        public async Task<Promoted.Delivery.Response> Deliver(Promoted.Delivery.Request req)
+        public void Dispose()
         {
+            Console.WriteLine("Waiting for pending tasks to complete...");
+            Task.WhenAll(pendingTasks).Wait();
+        }
+
+        private async Task<string> CallDelivery(Promoted.Delivery.Request req)
+        {
+            // Protobuf -> JSON.
+            string json = formatter.Format(req);
+            using var content = new StringContent(json, Encoding.UTF8, "application/json");
+            using HttpResponseMessage response = await deliveryHttpClient.PostAsync(deliveryEndpoint, content);
             try
             {
-                // Protobuf -> JSON
-                string json = formatter.Format(req);
-
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                HttpResponseMessage response = await httpClient.PostAsync(deliveryEndpoint, content);
                 // This throws if the request was unsuccessful.
                 response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-
-                // JSON -> Protobuf
-                return (Promoted.Delivery.Response)parser.Parse(responseBody, Promoted.Delivery.Response.Descriptor);
+                return await response.Content.ReadAsStringAsync();
             }
             catch (TaskCanceledException ex)
             {
                 // TODO(james): Proper timeout behavior.
-                Console.WriteLine($"Request timed out: {ex.Message}");
+                Console.WriteLine($"Delivery request timed out: {ex.Message}");
                 return null;
             }
             catch (Exception ex)
             {
                 // TODO(james): Proper fallback behavior.
-                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.WriteLine($"Delivery request failed: {ex.Message}");
                 return null;
             }
+        }
+
+        private void CallMetrics()
+        {
+            // TODO(james): Fix the event C# namespace in schema.
+            var log_req = new Event.LogRequest();
+            string json = formatter.Format(log_req);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            Task continuation = metricsHttpClient.PostAsync(metricsEndpoint, content).ContinueWith(task =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    using HttpResponseMessage response = task.Result;
+                    try
+                    {
+                        response.EnsureSuccessStatusCode();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Metrics request failed: {ex.Message}");
+                    }
+                }
+                // Timeouts are indicated here.
+                else if (task.IsFaulted || task.IsCanceled)
+                {
+                    Console.WriteLine($"Metrics request could not be completed: {task.Exception}");
+                }
+                // Remove the completed task from our list of pending tasks.
+                pendingTasks.TryTake(out _);
+
+                content.Dispose();
+            });
+            // Store these to try and ensure they get completed.
+            pendingTasks.Add(continuation);
+        }
+
+        public async Task<Promoted.Delivery.Response> Deliver(Promoted.Delivery.Request req)
+        {
+            string resp = await CallDelivery(req);
+            CallMetrics();
+            // JSON -> Protobuf.
+            return (Promoted.Delivery.Response)parser.Parse(resp, Promoted.Delivery.Response.Descriptor);
         }
     }
 }
