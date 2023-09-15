a32158d9b61f1ab3422de62a0eb3591e2c486a97
James Hill <james@promoted.ai>
2023-09-15 09:49:56 -0700

    base

---
+++ src/lib/DeliveryClientOptions.cs
@@ -0,0 +1,20 @@
+namespace Promoted.Lib
+{
+    public class DeliveryClientOptions
+    {
+        private float _shadowTrafficRate;
+
+        public float ShadowTrafficRate
+        {
+            get { return _shadowTrafficRate; }
+            set
+            {
+                if (value < 0 || value > 1)
+                {
+                    throw new ArgumentException("shadowTrafficRate must be between 0 and 1, inclusive.");
+                }
+                _shadowTrafficRate = value;
+            }
+        }
+    }
+}
