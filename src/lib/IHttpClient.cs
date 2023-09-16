8b5a7f83b473ec5b23871662795f3ff78533ffa2
James <113625718+prm-james-hill@users.noreply.github.com>
2023-09-16 13:45:27 -0700

    Refactor fire-and-forget execution logic out of DeliveryClient (#11)

--- src/lib/IHttpClient.cs
+++
@@ -1,9 +0,0 @@
-// This is just for dependency injection into DeliveryClient.
-
-namespace Promoted.Lib
-{
-    public interface IHttpClient
-    {
-        public Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content);
-    }
-}
---
+++ src/lib/OwningCallbackHttpClient.cs
@@ -0,0 +1,44 @@
+// In addition to implementing ICallbackHttpClient, this will:
+// 1. Take ownership of callback continuations
+// 2. Ensure that they do finish
+
+using NLog;
+using System.Collections.Concurrent;
+
+namespace Promoted.Lib
+{
+    // Disposable to ensure all pending tasks finish.
+    public class OwningCallbackHttpClient : ICallbackHttpClient, IDisposable
+    {
+        private readonly ConcurrentBag<Task> _pendingTasks = new ConcurrentBag<Task>();
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public OwningCallbackHttpClient(string apiKey, int timeoutMillis)
+        {
+            _httpClient.DefaultRequestHeaders.Add("x-api-key", apiKey);
+            _httpClient.Timeout = TimeSpan.FromMilliseconds(timeoutMillis);
+        }
+
+        public void Dispose()
+        {
+            Task.WhenAll(_pendingTasks).Wait();
+        }
+
+        public Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content)
+        {
+            return _httpClient.PostAsync(requestUri, content);
+        }
+
+        public void PostAsync(string requestUri, HttpContent content, Action<Task<HttpResponseMessage>> callback)
+        {
+            Task continuation = _httpClient.PostAsync(requestUri, content).ContinueWith(task =>
+            {
+                callback?.Invoke(task);
+                // Remove the completed task from our list of pending tasks.
+                _pendingTasks.TryTake(out _);
+            });
+            // Store these to ensure they get completed.
+            _pendingTasks.Add(continuation);
+        }
+    }
+}
